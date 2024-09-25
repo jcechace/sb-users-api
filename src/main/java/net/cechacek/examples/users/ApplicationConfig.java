c9ad0dbf84797940b6e066ac0a7cbc7f73ca866d
Jakub Cechacek <jcechace@gmail.com>
2024-09-25 11:22:53 +0200

    Conditionally creating pre-defined user

--- src/main/java/net/cechacek/examples/users/ApplicationConfig.java
+++ src/main/java/net/cechacek/examples/users/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package net.cechacek.examples.users;
 
 import lombok.RequiredArgsConstructor;
-import net.cechacek.examples.users.domain.User;
+import net.cechacek.examples.users.persistence.access.UserRepository;
+import net.cechacek.examples.users.services.domain.User;
 import net.cechacek.examples.users.security.JwtProperties;
 import net.cechacek.examples.users.services.UserService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -25,7 +27,8 @@ class ApplicationConfig {
     }
 
     @Bean
-    public CommandLineRunner run(UserService userService) {
+    @ConditionalOnProperty(name = "app.init-db", havingValue = "true")
+    public CommandLineRunner run(UserService userService, UserRepository userRepository) {
         return args -> {
             userService.create(new User(null, "John Doe", "john@example.com", "secret"));
         };
