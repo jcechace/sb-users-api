a8531700a45ed548c146c101e91982bbf5639bda
Jakub Cechacek <jcechace@gmail.com>
2024-09-25 11:23:48 +0200

    Reenabled test-mode application class

--- src/test/java/net/cechacek/examples/users/TestSbUsersApiApplication.java
+++ src/test/java/net/cechacek/examples/users/TestSbUsersApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestSbUsersApiApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.from(SbUsersApiApplication::main).with(TestcontainersConfiguration.class).run(args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.from(SbUsersApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
 
 }
